45d25d0bcbcf621d7c9efe0d378104263cb7c2d8
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-03-23 16:15:15 +0200

    Add MSNFI-only variant 08_msnfi_abf

---
+++ zsetup/08_msnfi_abf.bat
@@ -0,0 +1,1 @@
+call zig4.exe -r 08_msnfi_abf/08_msnfi_abf.dat 08_msnfi_abf/08_msnfi_abf.spp output/08_msnfi_abf/result_08_msnfi_abf.txt 0 0 1 0 --grid-output-formats=compressed-img --image-output-formats=png
