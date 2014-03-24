85eda8318fb98ba02442cb2498b7fcfcd816a046
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-03-24 19:15:44 +0200

    Add MSNFI-only variant 16_msnfi_abf_pe_w_nosfc

---
+++ zsetup/16_msnfi_abf_pe_w_nosfc.bat
@@ -0,0 +1,1 @@
+call zig4.exe -r 16_msnfi_abf_pe_w_nosfc/16_msnfi_abf_pe_w_nosfc.dat 16_msnfi_abf_pe_w_nosfc/16_msnfi_abf_pe_w_nosfc.spp output/16_msnfi_abf_pe_w_nosfc/result_16_msnfi_abf_pe_w_nosfc.txt 0 0 1 0 --grid-output-formats=compressed-img --image-output-formats=png
