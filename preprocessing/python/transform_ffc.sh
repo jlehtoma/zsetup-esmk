f211e5b3a8fa4d9f06af674a67af8ff985226f0d
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-06 19:11:52 +0300

    Fix correct tree spp field

--- preprocessing/python/transform_ffc.sh
+++ preprocessing/python/transform_ffc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python index_transform.py  -i ../../data/msnfi/segments/ -o ../../data/msnfi/indices -p ../data/parameters-esmk.csv -v -l MLVMI_PUULAJI
+python index_transform.py  -i ../../data/ffc/ositteet/ -o ../../data/ffc/indeksi -p ../data/parameters-esmk.csv -v -l IPUULAJI
