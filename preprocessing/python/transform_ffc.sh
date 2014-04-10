26a5226f89bc12d3bfd2ab3823c7a59e87a70fc8
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-10 08:40:23 +0300

    Fix tree spp field names in transformation scripts

--- preprocessing/python/transform_ffc.sh
+++ preprocessing/python/transform_ffc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python index_transform.py  -i ../../data/ffc/ositteet/ -o ../../data/ffc/indeksi -p ../data/parameters-esmk.csv -v -l IPUULAJI
+python index_transform.py  -i ../../data/ffc/ositteet/ -o ../../data/ffc/indeksi/test -p ../data/parameters-esmk.csv -v -l MK_PUULAJI
--- preprocessing/python/transform_ffps.sh
+++ preprocessing/python/transform_ffps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python index_transform.py  -i ../../data/ffps/ositteet/ -o ../../data/ffps/indeksi -p ../data/parameters-esmk.csv -v -l PUULAJI
+python index_transform.py  -i ../../data/ffps/ositteet/ -o ../../data/ffps/indeksi -p ../data/parameters-esmk.csv -v -l MH_PUULAJI
