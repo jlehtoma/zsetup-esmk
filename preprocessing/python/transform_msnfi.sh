5521a07dec948da7d93df3a561b1ec8563c30cee
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-04-11 17:58:57 +0300

    Fix paths

--- preprocessing/python/transform_msnfi.sh
+++ preprocessing/python/transform_msnfi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python index_transform.py  -i ../../data/msnfi/segments/ -o ../../data/msnfi/indices -p ../data/parameters-esmk.csv -v -l MLVMI_PUULAJI
+python index_transform.py  -i ../../data/msnfi/segment/partial/20/ositteet -o ../../data/msnfi/segment/partial/20/indeksi/ositteet -p ../data/parameters-esmk.csv -v -l MLVMI_PUULAJI -f img
