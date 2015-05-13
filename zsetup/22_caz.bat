515501808c2801648de59f0cad8d0c947e9afa8d
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-05-13 11:23:44 +0300

    Remove variant 22-24
    These were created to check the problem with Zonation threading.

--- zsetup/22_caz.bat
+++
@@ -1,1 +0,0 @@
-call zig4.exe -r 22_caz/22_caz.dat 22_caz/22_caz.spp output/22_caz/result_22_caz.txt 0 0 1 0 --grid-output-formats=compressed-img --image-output-formats=png
--- zsetup/23_caz_uc.bat
+++
@@ -1,1 +0,0 @@
-call zig4.exe -r 23_caz_uc/23_caz_uc.dat 23_caz_uc/23_caz_uc.spp output/23_caz_uc/result_23_caz_uc.txt 0.5 0 1 0 --grid-output-formats=compressed-img --image-output-formats=png
